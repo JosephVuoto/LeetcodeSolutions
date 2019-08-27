a4e017ecfddef99e30aca0c37468c8a0bce90ef6
JosephVuoto <joseph.vuoto@gmail.com>
2019-08-27 13:53:43 +1000

    update

---
+++ src/com/xieyangzhe/com/s200/Solution141.java
@@ -0,0 +1,42 @@
+package com.xieyangzhe.com.s200;
+
+import com.xieyangzhe.com.common.ListNode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Yangzhe Xie
+ * @date 2019-08-27
+ */
+
+public class Solution141 {
+    public boolean hasCycle(ListNode head) {
+        Set<ListNode> nodeSet = new HashSet<>();
+        while (head != null) {
+            if (nodeSet.contains(head)) {
+                return true;
+            } else {
+                nodeSet.add(head);
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    public boolean hasCycle1(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return true;
+    }
+}
