0c9c366f7ffa26a51f117b602f44734a576d8adf
JosephVuoto <joseph.vuoto@gmail.com>
2019-12-02 21:22:38 +0800

    solution060

--- src/com/xieyangzhe/com/Main.java
+++ src/com/xieyangzhe/com/Main.java
@@ -1,8 +1,11 @@
 package com.xieyangzhe.com;
 
+import com.xieyangzhe.com.s100.Solution060;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Solution060 solution060 = new Solution060();
+        System.out.println(solution060.getPermutation(3, 3));
     }
 }
