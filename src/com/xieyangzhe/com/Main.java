7970a581c206f98dad1fa907f8d8fd4a8d0ce3c7
JosephVuoto <joseph.vuoto@gmail.com>
2019-12-04 21:58:00 +0800

    update

--- src/com/xieyangzhe/com/Main.java
+++ src/com/xieyangzhe/com/Main.java
@@ -1,11 +1,14 @@
 package com.xieyangzhe.com;
 
+import com.xieyangzhe.com.common.ListNode;
 import com.xieyangzhe.com.s100.Solution060;
+import com.xieyangzhe.com.s100.Solution061;
 
 public class Main {
 
     public static void main(String[] args) {
-        Solution060 solution060 = new Solution060();
-        System.out.println(solution060.getPermutation(3, 3));
+        Solution061 solution061 = new Solution061();
+        ListNode node = ListNode.fromList(1, 2, 3, 4, 5, 6);
+        System.out.println(solution061.rotateRight(node, 2).toList());
     }
 }
