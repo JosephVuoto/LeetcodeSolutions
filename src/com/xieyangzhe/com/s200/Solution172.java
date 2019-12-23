22136b4a22d661bc0f65025cc1cda25582749e97
JosephVuoto <joseph.vuoto@gmail.com>
2019-12-23 11:47:59 +0800

    172

---
+++ src/com/xieyangzhe/com/s200/Solution172.java
@@ -0,0 +1,30 @@
+package com.xieyangzhe.com.s200;
+
+/**
+ * @author Yangzhe Xie
+ * @date 23/12/19
+ */
+public class Solution172 {
+    //172. Factorial Trailing Zeroes
+    //Given an integer n, return the number of trailing zeroes in n!.
+    //
+    //Example 1:
+    //
+    //Input: 3
+    //Output: 0
+    //Explanation: 3! = 6, no trailing zero.
+    //Example 2:
+    //
+    //Input: 5
+    //Output: 1
+    //Explanation: 5! = 120, one trailing zero.
+    //Note: Your solution should be in logarithmic time complexity.
+    public int trailingZeroes(int n) {
+        int result = 0;
+        while (n != 0) {
+            result ++;
+            n /= 5;
+        }
+        return result;
+    }
+}
