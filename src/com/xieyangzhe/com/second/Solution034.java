2693989990734893db2610f35a4da844d7951308
JosephVuoto <joseph.vuoto@gmail.com>
2020-03-14 22:15:42 +1100

    update

---
+++ src/com/xieyangzhe/com/second/Solution034.java
@@ -0,0 +1,63 @@
+package com.xieyangzhe.com.second;
+
+/**
+ * @author Yangzhe Xie
+ * @date 14/3/20
+ */
+//34. Find First and Last Position of Element in Sorted Array
+//Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+//
+//Your algorithm's runtime complexity must be in the order of O(log n).
+//
+//If the target is not found in the array, return [-1, -1].
+//
+//Example 1:
+//
+//Input: nums = [5,7,7,8,8,10], target = 8
+//Output: [3,4]
+//Example 2:
+//
+//Input: nums = [5,7,7,8,8,10], target = 6
+//Output: [-1,-1]
+public class Solution034 {
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirst(nums, target);
+        result[1] = findLast(nums, target);
+        return result;
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int index = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+            if (nums[mid] == target) {
+                index = mid;
+            }
+        }
+        return index;
+    }
+}
---
+++ src/com/xieyangzhe/com/second/Solution035.java
@@ -0,0 +1,22 @@
+package com.xieyangzhe.com.second;
+
+/**
+ * @author Yangzhe Xie
+ * @date 14/3/20
+ */
+public class Solution035 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+}
