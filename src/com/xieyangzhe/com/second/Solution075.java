a6a7bf6abd7c6cf6217ec3be5cc64cf6804dc5ad
JosephVuoto <joseph.vuoto@gmail.com>
2020-03-18 11:12:32 +1100

    update

---
+++ src/com/xieyangzhe/com/second/Solution075.java
@@ -0,0 +1,26 @@
+package com.xieyangzhe.com.second;
+
+/**
+ * @author Yangzhe Xie
+ * @date 18/3/20
+ */
+public class Solution075 {
+    public void sortColors(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        for (int i = 0; i <= right; i++) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right--;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
