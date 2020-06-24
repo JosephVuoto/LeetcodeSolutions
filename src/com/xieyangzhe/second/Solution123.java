1eb4e0bef392194b048dff00e1abc557a471a51c
JosephVuoto <joseph.vuoto@gmail.com>
2020-06-24 22:52:51 +1000

    update

--- src/com/xieyangzhe/second/Solution123.java
+++ src/com/xieyangzhe/second/Solution123.java
@@ -34,12 +34,14 @@ public class Solution123 {
         int sell1 = 0, sell2 = 0, buy1 = Integer.MIN_VALUE, buy2 = Integer.MIN_VALUE;
         for (int price : prices) {
             buy1 = Math.max(buy1, -price);
-            sell1 = Math.max(sell1, buy1 + price);
+            sell1 = Math.max(sell1, price + buy1);
             buy2 = Math.max(buy2, sell1 - price);
             sell2 = Math.max(sell2, buy2 + price);
         }
         return sell2;
     }
 
-
+    public static void main(String[] args) {
+        System.out.println(new Solution123().maxProfit(new int[]{1,2,4,2,5,7,2,4,9,0}));
+    }
 }
